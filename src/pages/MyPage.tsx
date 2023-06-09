0cb41d00bbf5d9cedab1d48f47e49fc67ed003a8
karinaquenta <karinaxquenta@gmail.com>
2023-06-09 11:47:39 -0700

    button update

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import  { ArticleContext }  from "../module/LikedProvider";
 import HeadlinesContainer from "../components/HeadlinesContainer";
 import Footer from "../components/Footer";
+import { Link } from 'react-router-dom';
 
 
 export default function MyPage() {
@@ -11,6 +12,17 @@ export default function MyPage() {
         <div className="mypage-page">
         <br/>
         <h1>My NewsFlow Page</h1>
+        <br/>
+             
+        <Link to="/ln" className="cta-link">
+                <button className="cta-button">Click here for Live Top Headlines!</button>
+              </Link>
+              <Link to="/news" className="cta-link">
+                <button className="cta-button">Click here to browse!</button>
+              </Link>
+              <br/>
+              <br/>
+              <br/>
         <HeadlinesContainer headlines={likedArticles} />
         <br/>
               <br/>
